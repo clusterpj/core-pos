3d662a4403a792051fa648dc9e8fb9ec19d24add
Pedro Jimenez <jisgore@gmail.com>
2024-11-04 10:52:41 -0400

    Fixed Orders Button

--- src/views/pos/components/PosFooter.vue
+++ src/views/pos/components/PosFooter.vue
@@ -4,7 +4,7 @@
     <div class="d-flex gap-2 justify-space-between w-100">
       <!-- Order Type Actions -->
       <div class="d-flex gap-2">
-        <held-orders-modal :disabled="isDisabled" />
+        <held-orders-modal :disabled="false" />
         <dine-in-modal :disabled="isDisabled" />
         <to-go-modal :disabled="isDisabled" />
         <delivery-modal :disabled="isDisabled" />
